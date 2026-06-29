8cf48c056ee6dcdb6d2d126180b4ef469075aa15
satendra <satendrakr.7@gmail.com>
2026-06-29 11:00:55 +0530

    setup mongoDB connection

--- config/db.js
+++ config/db.js
@@ -1,5 +1,14 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    
-}
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.log("Database connection failed");
+    console.error(error.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
