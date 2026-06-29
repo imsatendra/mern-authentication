c9526d61640dcb458e25c113391d8e62cc50621d
satendra <satendrakr.7@gmail.com>
2026-06-29 12:21:55 +0530

    Implement signup validation

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,11 +1,26 @@
-exports.signup = (req , res) => {
+const bcrypt = require("bcrypt");
+const User = require("../models/user.model");
 
-}
+exports.signup = async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
+    }
 
-exports.login = (req , res) => {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(409).json({
+        success: false,
+        message: "User already exists",
+      });
+    }
+  } catch (err) {}
+};
 
-}
+exports.login = (req, res) => {};
 
-exports.logout = (req , res) => {
-    
-}
+exports.logout = (req, res) => {};
