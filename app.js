7819a3e04f5584138b6101eaed8ca651b485a619
satendra <satendrakr.7@gmail.com>
2026-06-29 12:34:47 +0530

    update app routes

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const authRoutes = require("./routes/auth.routes");
 const app = express();
 
 app.use(express.json());
-app.use("/api/v1", authRoutes);
+app.use("/api/v1/auth", authRoutes);
 module.exports = app;
